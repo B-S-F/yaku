92a0c0e0e8c75f91667b42049ba9dd4cbecf040b
Frank Bernhardt <fra.bernhardt@googlemail.com>
2024-11-13 10:25:55 +0100

    fix: new version of commons libs synced

--- qg-api-service/api-commons-lib/src/stream-utils.ts
+++ qg-api-service/api-commons-lib/src/stream-utils.ts
@@ -14,6 +14,7 @@ export async function streamToString(stream: Readable): Promise<string> {
 export function decodeBufferToUTF8EncodedString(data: Buffer): string {
   try {
     return new TextDecoder('utf8', { fatal: true }).decode(data)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (err) {
     return undefined
   }
