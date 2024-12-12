d1100199b20d40a205a95796afd1767f9359f955
Florian Bufler <94462700+fbufler@users.noreply.github.com>
2024-12-12 12:48:54 +0100

    fix: migrate leftover yaku-ui unit tests (#111)
    Signed-off-by: BFF2FE <Florian.Bufler@de.bosch.com>

--- yaku-ui/qg-service-ui/src/utils/parseGithubUrl.spec.ts
+++ yaku-ui/qg-service-ui/src/utils/parseGithubUrl.spec.ts
@@ -2,33 +2,33 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { parseGithubUrl } from './parseGithubUrl'
+import { parseGithubUrl } from "./parseGithubUrl";
 
-describe('parseGithubUrl', () => {
-  it('returns the elements from a simple link to the repository', () => {
-    expect(parseGithubUrl('https://github.com/B-S-F/yaku/')).toStrictEqual({
+describe("parseGithubUrl", () => {
+  it("returns the elements from a simple link to the repository", () => {
+    expect(parseGithubUrl("https://github.com/B-S-F/yaku/")).toStrictEqual({
       ok: true,
-      org: 'B-S-F',
-      project: 'yaku',
-      repositoryUrl: 'https://github.com/B-S-F/yaku/',
-    })
-  })
+      org: "B-S-F",
+      project: "yaku",
+      repositoryUrl: "https://github.com/B-S-F/yaku",
+    });
+  });
 
-  it('returns the elements from a PR', () => {
+  it("returns the elements from a PR", () => {
     expect(
-      parseGithubUrl('https://github.com/B-S-F/yaku/pull/71'),
+      parseGithubUrl("https://github.com/B-S-F/yaku/pull/71")
     ).toStrictEqual({
       ok: true,
-      org: 'B-S-F',
-      project: 'yaku',
-      repositoryUrl: 'https://github.com/B-S-F/yaku/',
-    })
-  })
+      org: "B-S-F",
+      project: "yaku",
+      repositoryUrl: "https://github.com/B-S-F/yaku",
+    });
+  });
 
-  it('returns an error if it is not a github link', () => {
-    expect(parseGithubUrl('https://example.com/')).toStrictEqual({
+  it("returns an error if it is not a github link", () => {
+    expect(parseGithubUrl("https://example.com")).toStrictEqual({
       ok: false,
-      message: 'The github repository is not recognized from the URL.',
-    })
-  })
-})
+      message: "The github repository is not recognized from the URL.",
+    });
+  });
+});
